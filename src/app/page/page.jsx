bbe9c8370bb673c3700d8100a99bf90aa1d28a32
nexarphantom <marufhasanbr@gmail.com>
2026-04-19 21:01:56 +0600

    complete routing

---
+++ src/app/page/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageSide = () => {
+  return (
+    <div>
+      <h1>Page are comming soon</h1>
+    </div>
+  );
+};
+
+export default PageSide;
---
+++ src/app/posts/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const pagePost = () => {
+  return (
+    <div>
+      <h1>Posts are comming soon</h1>
+    </div>
+  );
+};
+
+export default pagePost;
