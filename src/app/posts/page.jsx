0ad0254adb03727f7acf5ed054196f322a75e8bf
nexarphantom <marufhasanbr@gmail.com>
2026-04-19 22:21:49 +0600

    complete routing section

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
-const pagePost = () => {
+// const getPost = async () => {
+//   try {
+//     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//     return res.json();
+//   } catch (error) {
+//     throw new error("Data isn't loaded");
+//   }
+// };
+
+const pagePost = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) {
+    throw new Error("data inn't loaded");
+  }
+  const posts = await res.json();
+  // const posts = await getPost();
   return (
     <div>
-      <h1>Posts are comming soon</h1>
+      <h1>Posts are comming soon : {posts.length}</h1>
     </div>
   );
 };
